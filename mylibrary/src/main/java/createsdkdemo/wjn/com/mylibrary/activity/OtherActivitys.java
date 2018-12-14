21e8dee943f19e906ea5a9dbcd94b2dd474885ff
wujna <tmac324421@126.com>
2018-12-14 16:29:06 +0800

    commit message

--- mylibrary/src/main/java/createsdkdemo/wjn/com/mylibrary/activity/OtherActivitys.java
+++ mylibrary/src/main/java/createsdkdemo/wjn/com/mylibrary/activity/OtherActivitys.java
@@ -2,6 +2,9 @@ package createsdkdemo.wjn.com.mylibrary.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import createsdkdemo.wjn.com.mylibrary.R;
 
@@ -11,6 +14,13 @@ public class OtherActivitys extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otheractivitys);
+
+        ImageView imageView=findViewById(R.id.activity_otheractivitys_imageview);
+        String url="http://i.imgur.com/DvpvklR.png";
+        Glide.with(this)
+                .load(url)
+                .into(imageView);
+
     }
 
 }
